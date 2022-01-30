bccecf85fa8d0d767799ca4910e3088cfb4fc278
Prashant Gorade <prashant.gorade@gmail.com>
2022-01-31 00:11:09 +0530

    first commit

--- routes/item.py
+++ routes/item.py
@@ -4,7 +4,6 @@ from config import item
 from schemas import Item
 import sqlalchemy
 
-
 item_routes = APIRouter()
 
 
@@ -19,8 +18,19 @@ def get_item():
 @item_routes.post("/item")
 def add_item(i: Item):
     with engine.connect() as connection:
-        query = sqlalchemy.insert(item).values((i.item_id, i.item_name, i.item_price, i.item_stock,i.item_image_url))
+        query = sqlalchemy.insert(item).values((i.id, i.name, i.price, i.stock, i.image_url))
         connection.execute(query)
-        query = sqlalchemy.select([item])
+        query = sqlalchemy.select(item)
         results = connection.execute(query).fetchall()
     return results[-1]
+
+
+@item_routes.delete("/item/{id}")
+def delete_item(id):
+    with engine.connect() as connection:
+        query = sqlalchemy.delete(item).where(item.c.id == id)
+        connection.execute(query)
+
+        query = sqlalchemy.select(item)
+        results = connection.execute(query).fetchall()
+    return results
--- schemas/item.py
+++ schemas/item.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
-    item_id: int
-    item_name: str
-    item_price: float
-    item_stock: int
-    item_image_url: str
+    id: int
+    name: str
+    price: float
+    stock: int
+    image_url: Optional[str]
