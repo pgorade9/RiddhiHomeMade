645465b1af8ba73cceb3a0b3aefeeaad01b0b8d0
Prashant Gorade <prashant.gorade@gmail.com>
2022-02-20 10:14:39 +0530

    first fastapi commit

--- routes/user.py
+++ routes/user.py
@@ -3,19 +3,16 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import schemas
-from config import engine, Base
 from config.db import SessionLocal
-from utils.user import crud
+from utils.user import crud_user
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 user_routes = APIRouter()
 
 
-
-
 # Dependency
 def get_db():
-    print("SessionLocal : ",SessionLocal)
+    print("SessionLocal : ", SessionLocal)
     db = SessionLocal()
     try:
         yield db
@@ -29,25 +26,26 @@ async def token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 
 @user_routes.get("/user/{user_id}")
-def get_user(user_id: int, db: Session = Depends(get_db)) -> schemas.User:
-    return crud.get_user_by_id(user_id=user_id,db=db)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    return crud_user.get_user_by_id(user_id=user_id, db=db)
+
 
 @user_routes.get("/user")
 def get_users(db: Session = Depends(get_db)):
-    return crud.get_all_users(db=db)
+    return crud_user.get_all_users(db=db)
 
 
 @user_routes.post("/user")
 def add_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = crud_user.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return crud_user.create_user(db=db, user=user)
 
 
 @user_routes.delete("/user/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(get_db),token:str=Depends(oauth2_scheme)):
-    db_user = crud.get_user_by_id(db=db, user_id=user_id)
+def delete_user(user_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    db_user = crud_user.get_user_by_id(db=db, user_id=user_id)
     if not db_user:
         raise HTTPException(status_code=400, detail="User Not Found for the given User Id")
-    return crud.delete_user(db=db, user_id=user_id)
+    return crud_user.delete_user(db=db, user_id=user_id)
--- utils/user.py
+++ utils/user.py
@@ -1,15 +1,44 @@
-from passlib.context import CryptContext
+from fastapi import HTTPException
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
+from starlette import status
 
 import models
 import schemas
-from schemas import UserCreate
+from config import settings
 from utils import hashing
 
 
 class CRUD:
 
-    def create_user(self, db: Session, user: UserCreate) -> models.User:
+    def authenticate_user(self, db: Session, email: str, password: str):
+        db_user = db.query(models.User).filter(models.User.email == email).first()
+        if not db_user:
+            return {"User not recognized. Please register"}
+        elif not hashing.verify_password(password, db_user.hashed_password):
+            return {"Invalid Username or Password"}
+        return jwt.encode({"sub": email}, settings.SECRET, settings.ALGORITHM)
+
+    def get_current_user(self, token: str, db: Session):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        print("i am at get_current_user")
+        try:
+            token = token.split(None, 1)[1]
+            data = jwt.decode(token, settings.SECRET, settings.ALGORITHM)
+            email = data.get("sub")
+        except JWTError:
+            raise credentials_exception
+        print("decoded email = ", email)
+        user = db.query(models.User).filter(models.User.email == email).first()
+        if user is None:
+            raise credentials_exception
+        return schemas.User(id=user.id, email=user.email, name=user.name)
+
+    def create_user(self, db: Session, user: schemas.UserCreate) -> models.User:
         db_user = models.User(name=user.name, email=user.email,
                               hashed_password=hashing.get_password_hashed(user.password),
                               image_url=user.image_url)
@@ -41,4 +70,4 @@ class CRUD:
         return result
 
 
-crud = CRUD()
+crud_user = CRUD()
